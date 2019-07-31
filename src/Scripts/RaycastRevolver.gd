7e3fc970addb5f11827c02586f9f4f28f5ac7bcf
Doc McCoy <renan_ota@hotmail.com>
2019-07-31 20:19:50 -0300

    Melhorias na nova cena 4

--- src/Scripts/RaycastRevolver.gd
+++ src/Scripts/RaycastRevolver.gd
@@ -7,6 +7,16 @@ func _ready():
 	raycast.set_cast_to(Vector2(0, tamanho_tela.x))
 
 func _physics_process(delta):
+	# Sempre olhar para o mouse:
+	$Base.set_rotation(get_local_mouse_position().angle())
+	
+	# Correção quando o objeto está a direita do mouse:
+	if position.x < get_local_mouse_position().x:
+		$Base.set_scale(Vector2(1, 1))
+	else:
+		$Base.set_scale(Vector2(1, -1))
+		
+	
 	if Input.is_action_just_pressed("rewind_time"):
 		print("Bang!")
 		print(raycast.get_collider())
