712d06def2aae16180572f511cc39b7e454eb2ed
Doc McCoy <renan_ota@hotmail.com>
2019-08-01 20:48:49 -0300

    Finalização e aperfeiçoamento da cena 4

--- src/Scripts/Explosion.gd
+++ src/Scripts/Explosion.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	$Timer.start()
+	$AnimationPlayer.play("pew")
 
-func _on_Timer_timeout():
+func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
--- src/Scripts/RaycastRevolver.gd
+++ src/Scripts/RaycastRevolver.gd
@@ -2,26 +2,50 @@ extends Node2D
 
 onready var raycast : RayCast2D = get_node("Base/RayCast2D")
 var explosion = preload("res://Scenes/Explosion.tscn")
-
+var can_fire : bool = true
+# -------------------------------------------------------------------
 func _ready():
+	adjust_raycast_size()
+
+func _physics_process(delta):
+	look_to_the_mouse()
+	if Input.is_action_just_pressed("fire"):
+		if can_fire:
+			shot()
+		else:
+			shoot_fail()
+# -------------------------------------------------------------------
+func adjust_raycast_size():
 	var tamanho_tela = get_viewport().get_visible_rect().size
 	raycast.set_cast_to(Vector2(0, tamanho_tela.x))
 
-func _physics_process(delta):
-	# Sempre olhar para o mouse:
+func look_to_the_mouse():
 	$Base.set_rotation(get_local_mouse_position().angle())
-	
 	# Correção quando o objeto está a direita do mouse:
 	if position.x < get_local_mouse_position().x:
 		$Base.set_scale(Vector2(1, 1))
 	else:
 		$Base.set_scale(Vector2(1, -1))
 
-	if Input.is_action_just_pressed("fire"):
-		var acertou = raycast.get_collider()
-		if acertou:
-			var clone_explosion = explosion.instance()
-			var ponto_de_colisao = raycast.get_collision_point()
-			# Criar explosao
-			clone_explosion.set_global_position(ponto_de_colisao)
-			get_parent().get_parent().add_child(clone_explosion)
+func shot():
+	can_fire = false
+	$RecoilTimer.start()
+	$BangSound.play()
+	var acertou = raycast.get_collider()
+	if acertou:
+		var clone_explosion = explosion.instance()
+		var ponto_de_colisao = raycast.get_collision_point()
+		var colidiu_com = raycast.get_collider()
+		var collider_groups = colidiu_com.get_groups()
+		if "enemy" in collider_groups:
+			print("hit enemy!")
+		# Criar explosao
+		clone_explosion.set_global_position(ponto_de_colisao)
+		clone_explosion.set_rotation_degrees(rand_range(0, 360))
+		get_parent().get_parent().add_child(clone_explosion)
+
+func shoot_fail():
+	$ClickSound.play()
+
+func _on_RecoilTimer_timeout():
+	can_fire = true
