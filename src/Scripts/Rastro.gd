ec4bee5be4131b1a22d7e48862f05bd37cc65d02
Renan <renan_ota@hotmail.com>
2019-08-06 18:49:50 -0300

    Melhoria no Rastro: A animação agora é feita por animação ao invés de estar no _proces()

--- src/Scripts/Rastro.gd
+++ src/Scripts/Rastro.gd
@@ -4,10 +4,8 @@ func setup(origem : Vector2, destino : Vector2) -> void:
 	$Line2D.add_point(origem)
 	$Line2D.add_point(destino)
 
-func _process(delta):
-	# Fade no rastro até desaparecer
-	var alpha = modulate.a
-	alpha -= 0.1
-	modulate = Color(modulate.r, modulate.g, modulate.b, alpha)
-	if alpha < 0:
-		queue_free()
+func _ready():
+	$AnimationPlayer.play("Dissolver")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	queue_free()
