71da34ccdc7fcad6f704255cc9e560e4fbd867e9
Renan Santana <renan_ota@hotmail.com>
2020-02-13 22:51:18 -0300

    Ajustes na cena 8

--- src/Scripts/PlayerTween.gd
+++ src/Scripts/PlayerTween.gd
@@ -3,20 +3,27 @@ extends KinematicBody2D
 export(float) var squash
 var was_grounded = false
 var velocity = Vector2.ZERO
-var jump_height = 300.0
+var jump_height = 500.0
+var speed = 300.0
 onready var tween = $Tween
 onready var mask = $Sprite
-const gravity = 7.0
+const gravity = 12.0
 
 func _physics_process(delta):
-	if is_on_floor() && !was_grounded:
+	if is_on_floor() and !was_grounded:
 		squash_landed()
 	was_grounded = is_on_floor()
 	velocity.y += gravity
+	if Input.is_action_pressed("mv_right"):
+		velocity.x = speed
+	elif Input.is_action_pressed("mv_left"):
+		velocity.x = -speed
+	else:
+		velocity.x = 0
 	if Input.is_action_just_pressed("jump"):
 		was_grounded = false
-		velocity.y -= jump_height
 		squash_jump()
+		velocity.y -= jump_height
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 func squash_landed():
