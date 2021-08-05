5c33f224f5e0973fb2c6fb555946001132db189d
Renan Santana <renan_ota@hotmail.com>
2021-08-04 22:03:50 -0300

    Renomeia alguns arquivos

--- src/Scripts/BasicPlayer.gd
+++ src/Scripts/BasicPlayer.gd
@@ -1,29 +1,32 @@
 extends KinematicBody2D
 
+
 const UP = Vector2(0, -1)
 const GRAVITY = 15
 const JUMP = 500
 
 export var speed : int = 200
+
 var motion : Vector2 = Vector2()
 var can_control : bool = true
 
 onready var sprite : Sprite = $Sprite
 onready var animator : AnimationPlayer = $AnimationPlayer
+onready var pivot = $Pivot
 
 # Signal utilizado pelo script Camera2D para ajuste da câmera
 signal player_flipped(direction)
 
 
-func _physics_process(delta)->void:
+func _physics_process(delta) -> void:
 	if can_control:
 		motion.y += GRAVITY
 		motion = move_and_slide(fromInputsToMotion(), UP)
-	$Pivot.look_at(get_global_mouse_position())
+	pivot.look_at(get_global_mouse_position())
 	animate()
 
-func fromInputsToMotion()->Vector2:
 
+func fromInputsToMotion() -> Vector2:
 	if Input.is_action_pressed("mv_right"):
 		motion.x = speed
 	elif Input.is_action_pressed("mv_left"):
@@ -32,14 +35,14 @@ func fromInputsToMotion()->Vector2:
 		motion.x = 0
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		motion.y = -JUMP
-
 	return motion
 
-func animate()->void:
+
+func animate() -> void:
 	if motion.x != 0:
 		animator.play("Walking")
 	else:
-		animator.play("Idle")		
+		animator.play("Idle")
 	if !is_on_floor():
 		animator.play("Jumping")
 	if motion.x > 0 and sprite.flip_h:
