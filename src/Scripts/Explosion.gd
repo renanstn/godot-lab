fef2b5d1433e2f4f8ca71bddd64d3caded551851
Renan Santana <renan_ota@hotmail.com>
2021-08-04 22:08:19 -0300

    Limpa projeto

--- src/Scripts/Explosion.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	$AnimationPlayer.play("pew")
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	queue_free()
--- src/Scripts/Globals.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-# Tamanho global de um bloco dentro do jogo
-const UNIT_SIZE = 64
